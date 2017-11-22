7843d48f47bd5e734c584f8d3761350f3676eeda
pelpro <pelpro@yandex.ru>
2017-11-23 00:34:59 +0300

    first

---
+++ jsstask/app.js
@@ -0,0 +1,15 @@
+function AddingElement(st) {
+	var prop = $('<li><span></span> <button class="delete-button">Удалить</button></li>');
+	$('span',prop).text(st);
+	$('#root ul').append(prop);
+	$('.delete-but', el).click(function(deleete) {
+		$(this).parent().remove() });
+}
+$(function(){
+	$('#root').append('<ul></ul>');
+	$('#root').append('<input id="add_task_input">');
+	$('#root').append('<button id ="add_task">Добавить</button>');
+	$('#add_task').click(function(){
+	addElement($('#add_task_input').val())});
+	AddingElement("Сделать задание #3 по web-программированию");
+})   
